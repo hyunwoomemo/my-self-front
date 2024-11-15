78384a7730e5724595f5edaa25e6be034adc728e
hyemi <hmkim@spolive.comdfd>
2024-11-15 13:58:38 +0900

    create button 위치 수정

--- moim-moim/src/components/home/MeetingListContainer.tsx
+++ moim-moim/src/components/home/MeetingListContainer.tsx
@@ -45,7 +45,7 @@ const MeetingListContainer = () => {
           <MeetingList />
         </div>
       </div>
-      <div className="fixed bottom-32 w_sm:right-4 w_xl:right-56" onClick={() => router.push("/create")}>
+      <div className="fixed bottom-32 w_sm:right-4 w_xl:right-20" onClick={() => router.push("/create")}>
         <CreateButton />
       </div>
     </>
