5775ed8c7faf6985834210af40800e06ab7ae798
hyemi <hmkim@spolive.comdfd>
2024-11-18 14:56:13 +0900

    모임방 만들기 페이지 경로

---
+++ moim-moim/src/app/(moim)/create/write/page.tsx
@@ -0,0 +1,11 @@
+import PageHeader from "@/components/common/PageHeader";
+import CreateContainer from "@/components/meeting/create/CreateContainer";
+
+export default async function Page() {
+  return (
+    <>
+      <PageHeader title="모임 만들기" />
+      <CreateContainer />
+    </>
+  );
+}
