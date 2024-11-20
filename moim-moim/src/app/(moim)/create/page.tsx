0f1a6fa67944630cd123d733a27487fe5745ba47
hyemi <hmkim@spolive.comdfd>
2024-11-20 09:42:49 +0900

    로더 및 데이터 수정

--- moim-moim/src/app/(moim)/create/page.tsx
+++ moim-moim/src/app/(moim)/create/page.tsx
@@ -1,3 +1,4 @@
+import Loader from "@/components/common/Loader";
 import PageHeader from "@/components/common/PageHeader";
 import Category from "@/components/meeting/create/Category";
 
@@ -5,10 +6,14 @@ export default async function Page() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/moim/category`);
   const categories = await res.json();
 
+  if (!categories) {
+    return <Loader />;
+  }
+
   return (
     <>
       <PageHeader title="모임 만들기" />
-      <Category dataCategory1={categories.DATA.category1} dataCategory2={categories.DATA.category2} />
+      <Category dataCategory1={categories.data.category1} dataCategory2={categories.data.category2} />
     </>
   );
 }
