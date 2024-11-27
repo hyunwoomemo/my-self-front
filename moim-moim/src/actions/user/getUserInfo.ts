2a582a3304af2e900b20fe8782c1cb4d7260792f
hyemi <hmkim@spolive.comdfd>
2024-11-27 15:27:46 +0900

    유저 정보 불러오기

--- moim-moim/src/actions/user/getUserInfo.ts
+++ moim-moim/src/actions/user/getUserInfo.ts
@@ -1,23 +1,9 @@
-import { accountApi } from "@/app/api";
-import { cookies } from "next/headers";
+import { nextFetch } from "@/utils/fetch";
 
 export const getUserInfo = async () => {
-  const cookieStore = await cookies();
-  const token = await cookieStore.get("accessToken")?.value;
-
-  if (!token) return null;
   try {
-    const res = await accountApi.myInfo();
-    if (!res.ok) {
-      console.error(`요청 실패 코드: ${res.status}`);
-      const error = await res.json();
-      console.log(error);
-    }
-
-    const json = await res.json();
-    console.log("❤️?", json);
-    return json;
+    return await nextFetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user/myInfo`);
   } catch (error) {
-    console.error("api 호출 실패", error, "token", token);
+    console.error("api 호출 실패", error);
   }
 };
--- moim-moim/src/app/(moim)/moim/[moim_id]/chat/page.tsx
+++ moim-moim/src/app/(moim)/moim/[moim_id]/chat/page.tsx
@@ -1,13 +1,13 @@
+import { getUserInfo } from "@/actions/user/getUserInfo";
 import ChatContainer from "@/components/meeting/chat/ChatContainer";
 
 const Chat = async ({ params }: { params: { moim_id: string } }) => {
   const { moim_id } = await params;
-  // const userInfo = await getUserInfo();
-  // console.log("🔔🔔🔔", userInfo);
-
+  const userInfo = await getUserInfo();
+  console.log("userInfo", userInfo);
   return (
     <div className="flex h-screen flex-col">
-      <ChatContainer id={moim_id} />
+      <ChatContainer id={moim_id} userInfo={userInfo.data} />
     </div>
   );
 };
