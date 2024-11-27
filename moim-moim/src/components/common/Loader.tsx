663a40a50177f39dd6dcbca5aa849dbd324f2e66
hyemi <hmkim@spolive.comdfd>
2024-11-27 15:26:26 +0900

    loader 수정

--- moim-moim/src/components/common/Loader.tsx
+++ moim-moim/src/components/common/Loader.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 const Loader = () => {
   return (
-    <div className="fixed left-0 top-0 h-full w-full">
-      <div className="absolute left-1/2 top-1/2 mx-auto -translate-x-1/2 -translate-y-1/2">
+    <div className="fixed left-0 top-0 h-full w-full bg-transparent">
+      <div className="absolute left-1/2 top-1/2 mx-auto -translate-x-1/2 -translate-y-1/2 bg-transparent">
         {/* <div className="loader my-15 relative left-[-100px] block h-3 w-3 animate-shadowRolling rounded-full"></div> */}
-        <div>Loading...</div>
+        <Image src="/loading.gif" width={80} height={80} alt="loading..." />
       </div>
     </div>
   );
--- moim-moim/tailwind.config.ts
+++ moim-moim/tailwind.config.ts
@@ -32,39 +32,39 @@ const config: Config = {
         main: "linear-gradient(to right, #24C76D, #0da0c5)",
       },
       keyframes: {
-        shadowRolling: {
-          "0%": {
-            boxShadow:
-              "0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0)",
-          },
-          "12%": {
-            boxShadow:
-              "100px 0 #0da0c5, 0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0)",
-          },
-          "25%": {
-            boxShadow: "110px 0 #0da0c5, 100px 0 #0da0c5, 0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0)",
-          },
-          "36%": {
-            boxShadow: "120px 0 #24C76D, 110px 0 #24C76D, 100px 0 #24C76D, 0px 0 rgba(13,160,197, 0)",
-          },
-          "50%": {
-            boxShadow: "130px 0 #24C76D, 120px 0 #24C76D, 110px 0 #24C76D, 100px 0 #24C76D",
-          },
-          "62%": {
-            boxShadow: "200px 0 rgba(13,160,197, 0), 130px 0 #24C76D, 120px 0 #24C76D, 110px 0 #24C76D",
-          },
-          "75%": {
-            boxShadow: "200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0), 130px 0 #0da0c5, 120px 0 #0da0c5",
-          },
-          "87%": {
-            boxShadow:
-              "200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0), 130px 0 #0da0c5",
-          },
-          "100%": {
-            boxShadow:
-              "200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0)",
-          },
-        },
+        // shadowRolling: {
+        //   "0%": {
+        //     boxShadow:
+        //       "0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0)",
+        //   },
+        //   "12%": {
+        //     boxShadow:
+        //       "100px 0 #0da0c5, 0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0)",
+        //   },
+        //   "25%": {
+        //     boxShadow: "110px 0 #0da0c5, 100px 0 #0da0c5, 0px 0 rgba(13,160,197, 0), 0px 0 rgba(13,160,197, 0)",
+        //   },
+        //   "36%": {
+        //     boxShadow: "120px 0 #24C76D, 110px 0 #24C76D, 100px 0 #24C76D, 0px 0 rgba(13,160,197, 0)",
+        //   },
+        //   "50%": {
+        //     boxShadow: "130px 0 #24C76D, 120px 0 #24C76D, 110px 0 #24C76D, 100px 0 #24C76D",
+        //   },
+        //   "62%": {
+        //     boxShadow: "200px 0 rgba(13,160,197, 0), 130px 0 #24C76D, 120px 0 #24C76D, 110px 0 #24C76D",
+        //   },
+        //   "75%": {
+        //     boxShadow: "200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0), 130px 0 #0da0c5, 120px 0 #0da0c5",
+        //   },
+        //   "87%": {
+        //     boxShadow:
+        //       "200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0), 130px 0 #0da0c5",
+        //   },
+        //   "100%": {
+        //     boxShadow:
+        //       "200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0), 200px 0 rgba(13,160,197, 0)",
+        //   },
+        // },
       },
       animation: {
         shadowRolling: "shadowRolling 1s linear infinite",
