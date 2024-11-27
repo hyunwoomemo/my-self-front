4ed34cd67d70d6bbc44ed8e2adc3216d29c8d377
hyemi <hmkim@spolive.comdfd>
2024-11-27 15:28:15 +0900

    지역 선택 수정

--- moim-moim/src/app/(auth)/postcode/page.tsx
+++ moim-moim/src/app/(auth)/postcode/page.tsx
@@ -1,18 +1,36 @@
-import React from "react";
-import DaumPostcodeContainer from "./DaumPostcodeContainer";
+"use client";
+
+import { getUserInfo } from "@/actions/user/getUserInfo";
+import Button from "@/components/common/Button";
+import Region from "@/components/common/Region";
+import { Address } from "@/components/postcode";
+import { useSocket } from "@/hooks/useSocket";
+import { currentAreaAtom } from "@/store/area/atom";
+import { useSetAtom } from "jotai";
+import React, { useEffect, useState } from "react";
 
 export default function Page() {
+  const [address, setAddress] = useState<Address[]>([]);
+  const [addressKeyword, setAddressKeyword] = useState<string>("");
+  const [selectedArea, setSelectedArea] = useState("");
+  const { joinArea } = useSocket();
+  const setCurrentArea = useSetAtom(currentAreaAtom);
+
+  const handleClick = () => {
+    joinArea(selectedArea);
+    setCurrentArea(selectedArea);
+  };
+
   return (
-    <div>
-      <h1>우편번호 찾기</h1>
-      <DaumPostcodeContainer />
+    <div className="flex h-[calc(100vh-6rem)] flex-col justify-between p-5">
+      <Region
+        address={address}
+        addressKeyword={addressKeyword}
+        setAddress={setAddress}
+        setAddressKeyword={setAddressKeyword}
+        setSelectedArea={setSelectedArea}
+      />
+      <Button title="저장" disabled={!selectedArea} onClick={handleClick} />
     </div>
   );
 }
-
-// import React from "react";
-// import { SignContainer } from "./SignContainer";
-
-// export default function Page() {
-//   return <SignContainer />;
-// }
