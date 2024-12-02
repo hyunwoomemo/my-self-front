1f23da0caaca665a5ddd9631a719bb37c000be83
hyemi <hmkim@spolive.comdfd>
2024-12-02 15:58:07 +0900

    인트로 컴포넌트 분리

--- moim-moim/src/components/meeting/intro/IntroContainer.tsx
+++ moim-moim/src/components/meeting/intro/IntroContainer.tsx
@@ -3,27 +3,23 @@
 import Button from "@/components/common/Button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { TbCrown } from "react-icons/tb";
 import { GoHeartFill } from "react-icons/go";
 import { IoCloseOutline } from "react-icons/io5";
 import { SlSettings } from "react-icons/sl";
 import { useEffect, useState } from "react";
-import { CiHeart } from "react-icons/ci";
-import { PiUsersLight } from "react-icons/pi";
 import Loader from "@/components/common/Loader";
 import { useAtomValue } from "jotai";
 import { loadingAtom } from "@/store/common/atom";
 import { meetingDataAtom } from "@/store/meeting/data/atom";
 import { getMeetingData, useSocket } from "@/hooks/useSocket";
-import moment from "moment";
 import Empty from "@/components/common/Empty";
+import IntroData from "./IntroData";
 
 const IntroContainer = () => {
   const [imgNum, setImgNum] = useState<string | null>(null);
   const loading = useAtomValue(loadingAtom);
   const data = useAtomValue(meetingDataAtom) as getMeetingData;
   const router = useRouter();
-  const { joinMeeting, socket } = useSocket();
 
   // console.log("ss", socket?.connected);
   console.log("meetingData", data);
@@ -34,15 +30,6 @@ const IntroContainer = () => {
     console.log(formattedNum);
     setImgNum(formattedNum);
   }, []);
-
-  const handleClick = () => {
-    joinMeeting({
-      meetings_id: data.id,
-      region_code: "A02",
-      users_id: 125,
-      type: data.type,
-    });
-  };
 
   if (loading) {
     return <Loader />;
@@ -61,58 +48,7 @@ const IntroContainer = () => {
               <IoCloseOutline />
             </button>
           </div>
-
-          <div className="absolute z-10 flex h-full w-full flex-col gap-5 bg-black bg-opacity-60 px-5 py-5">
-            <div className="flex flex-1 flex-col justify-between">
-              <div className="flex flex-col items-center gap-6 pt-20">
-                <div className="flex flex-col items-center gap-2">
-                  <span className="w-fit rounded-full bg-semiPrimary px-4 py-2 text-xs font-bold">
-                    {data.category1_name}/{data.category2_name}
-                  </span>
-                  <h1 className="text-center text-3xl font-bold text-bg">{data.name}</h1>
-                </div>
-                <div className="flex flex-col gap-2">
-                  <div className="flex flex-col items-center gap-1">
-                    <div className="flex h-12 w-12 items-center justify-center rounded-full bg-point text-center text-3xl text-white shadow-lg">
-                      <TbCrown />
-                    </div>
-                    <div className="flex flex-col">
-                      <span className="font-bold text-white">{data.creator_name}</span>
-                      <span className="text-xs text-white">레벨</span>
-                    </div>
-                    <div className="flex gap-1">
-                      <div className="flex items-center">
-                        <CiHeart className="text-2xl text-white" />
-                        <span className="text-white">7</span>
-                      </div>
-                      <div className="font-thin text-white">·</div>
-                      <div className="flex items-center gap-1">
-                        <PiUsersLight className="text-2xl text-white" />
-                        <span className="text-white">
-                          {data.userCount}/{data.max_members}
-                        </span>
-                      </div>
-                    </div>
-                    <span className="text-sm font-bold text-semiPrimary">
-                      {data.region_code} · {moment(data.event_date).format("LL")} ·{" "}
-                      {moment(data.event_date).format("LT")}
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="scrollbar w-full overflow-y-auto whitespace-pre-wrap break-all rounded-xl bg-black bg-opacity-80 p-4 text-white h_sm:max-h-44 h_lg:max-h-64 h_xl:max-h-96">
-                {data.description}
-              </div>
-            </div>
-
-            {/* 하단 버튼 */}
-            <div className="flex gap-2">
-              <div className="rounded-lg bg-white p-5 text-3xl text-textGray">
-                <GoHeartFill />
-              </div>
-              <Button title={data.type === 3 ? "입장하기" : "입장 신청하기"} flex textSize="lg" onClick={handleClick} />
-            </div>
-          </div>
+          <IntroData enterIntro data={data} />
 
           {/* (dev) 임시로 랜덤 배경 처리 */}
           {imgNum && (
---
+++ moim-moim/src/components/meeting/intro/IntroData.tsx
@@ -0,0 +1,80 @@
+import Button from "@/components/common/Button";
+import { getMeetingData, useSocket } from "@/hooks/useSocket";
+import moment from "moment";
+import { CiHeart } from "react-icons/ci";
+import { GoHeartFill } from "react-icons/go";
+import { PiUsersLight } from "react-icons/pi";
+import { TbCrown } from "react-icons/tb";
+
+interface IntroDataProps {
+  data: getMeetingData;
+  enterIntro?: boolean;
+}
+
+const IntroData = ({ data, enterIntro }: IntroDataProps) => {
+  const { joinMeeting } = useSocket();
+
+  const handleClick = () => {
+    joinMeeting({
+      meetings_id: data.id,
+      region_code: "A02",
+      users_id: 125,
+      type: data.type,
+    });
+  };
+  return (
+    <div className={`absolute bottom-0 left-0 z-10 flex h-full w-full flex-col gap-5 bg-black bg-opacity-60 px-5 py-5`}>
+      <div className="flex flex-1 flex-col justify-between">
+        <div className="flex flex-col items-center gap-6 pt-20">
+          <div className="flex flex-col items-center gap-2">
+            <span className="w-fit rounded-full bg-semiPrimary px-4 py-2 text-xs font-bold">
+              {data.category1_name}/{data.category2_name}
+            </span>
+            <h1 className="text-center text-3xl font-bold text-bg">{data.name}</h1>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col items-center gap-1">
+              <div className="flex h-12 w-12 items-center justify-center rounded-full bg-point text-center text-3xl text-white shadow-lg">
+                <TbCrown />
+              </div>
+              <div className="flex flex-col">
+                <span className="font-bold text-white">{data.creator_name}</span>
+                <span className="text-xs text-white">레벨</span>
+              </div>
+              <div className="flex gap-1">
+                <div className="flex items-center">
+                  <CiHeart className="text-2xl text-white" />
+                  <span className="text-white">7</span>
+                </div>
+                <div className="font-thin text-white">·</div>
+                <div className="flex items-center gap-1">
+                  <PiUsersLight className="text-2xl text-white" />
+                  <span className="text-white">
+                    {data.userCount}/{data.max_members}
+                  </span>
+                </div>
+              </div>
+              <span className="text-sm font-bold text-semiPrimary">
+                {data.region_code} · {moment(data.event_date).format("LL")} · {moment(data.event_date).format("LT")}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="scrollbar w-full overflow-y-auto whitespace-pre-wrap break-all rounded-xl bg-black bg-opacity-80 p-4 text-white h_sm:max-h-44 h_lg:max-h-64 h_xl:max-h-96">
+          {data.description}
+        </div>
+      </div>
+      {/* 하단 버튼 */}
+      {enterIntro && (
+        <div className="flex gap-2">
+          <div className="rounded-lg bg-white p-5 text-3xl text-textGray">
+            <GoHeartFill />
+          </div>
+          <Button title={data.type === 3 ? "입장하기" : "입장 신청하기"} flex textSize="lg" onClick={handleClick} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default IntroData;
