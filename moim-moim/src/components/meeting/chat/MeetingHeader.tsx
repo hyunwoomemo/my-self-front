cb9bf8bafa1dd052ff38e83501d6047428fcb784
hyemi <hmkim@spolive.comdfd>
2024-12-02 15:57:15 +0900

    디벨롭된 컴포넌트 사용을 위한 수정

--- moim-moim/src/components/meeting/chat/MeetingHeader.tsx
+++ moim-moim/src/components/meeting/chat/MeetingHeader.tsx
@@ -7,15 +7,19 @@ import { getMeetingData } from "@/hooks/useSocket";
 import { loadingAtom } from "@/store/common/atom";
 import { meetingDataAtom } from "@/store/meeting/data/atom";
 import { useAtomValue } from "jotai";
-import { BiHomeSmile } from "react-icons/bi";
-import localFont from "next/font/local";
 import moment from "moment";
+import { FaBars } from "react-icons/fa6";
+import { useState } from "react";
+import SlideBar from "./SlideBar";
 
 const MeetingHeader = () => {
   const data = useAtomValue(meetingDataAtom) as getMeetingData;
   const loading = useAtomValue(loadingAtom);
+  const [show, setShow] = useState<boolean>(false);
 
-  // console.log("data", data);
+  const handleClickSideBar = () => {
+    setShow(!show);
+  };
 
   if (loading) {
     return <Loader />;
@@ -25,11 +29,7 @@ const MeetingHeader = () => {
       {data ? (
         <>
           <div className="sticky top-0 w-full border-b border-solid border-border bg-[rgba(255,255,255,0.5)] backdrop-blur-[2px]">
-            <PageHeader
-              title={data.name}
-              icon={<BiHomeSmile size={28} color="#33 3" />}
-              link={`/moim/${data.id}/intro`}
-            />
+            <PageHeader title={data.name} icon={<FaBars size={22} color="#333" />} onClick={handleClickSideBar} />
             <div className="flex justify-between bg-opacity-80 pb-5 pl-10 pr-5 w_sm:pb-3">
               <div className="flex flex-col">
                 <h3 className="font-tantan text-3xl">{moment(data.event_date).format("YYYY.MM.DD")}</h3>
@@ -39,6 +39,7 @@ const MeetingHeader = () => {
               </div>
             </div>
           </div>
+          <SlideBar />
         </>
       ) : (
         <Empty text="데이터가 없습니다." />
