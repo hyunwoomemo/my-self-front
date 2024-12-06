15765ddea449c88b182f4f05158b520ec389545e
hyemi <hmkim@spolive.comdfd>
2024-12-06 12:17:27 +0900

    로딩바 원복

--- moim-moim/src/components/common/Loader.tsx
+++ moim-moim/src/components/common/Loader.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 const Loader = () => {
   return (
-    <div className="absolute left-1/2 top-1/2 mx-auto -translate-x-1/2 -translate-y-1/2 bg-transparent">
-      {/* <div className="loader my-15 relative left-[-100px] block h-3 w-3 animate-shadowRolling rounded-full"></div> */}
-      <Image src="/loading.gif" width={80} height={80} alt="loading..." />
+    <div className="fixed left-0 top-0 h-full w-full bg-transparent">
+      <div className="absolute left-1/2 top-1/2 mx-auto -translate-x-1/2 -translate-y-1/2 bg-transparent">
+        {/* <div className="loader my-15 relative left-[-100px] block h-3 w-3 animate-shadowRolling rounded-full"></div> */}
+        <Image src="/loading.gif" width={80} height={80} alt="loading..." />
+      </div>
     </div>
   );
 };
